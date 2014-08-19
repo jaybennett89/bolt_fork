4049f5d0d55ece8fa11a371529fb1035ba94b125
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-08-19 12:43:01 +0200

    changed maps/prefabs name generation

--- src/bolt/bolt.editor/Compiler/BoltCompiler.CompileMaps.cs
+++ src/bolt/bolt.editor/Compiler/BoltCompiler.CompileMaps.cs
@@ -6,13 +6,17 @@ using UnityEngine;
 partial class BoltCompiler {
   public static void CompileMaps (BoltCompilerOperation op) {
     using (BoltSourceFile file = new BoltSourceFile(op.mapsFilePath)) {
-      file.EmitScope("public static class BoltMapNames", () => {
+      file.EmitScope("public static class BoltMaps", () => {
         for (int i = 0; i < EditorBuildSettings.scenes.Length; ++i) {
           var s = EditorBuildSettings.scenes[i];
+          file.EmitLine("public static readonly string {0} = \"{0}\";", Path.GetFileNameWithoutExtension(s.path));
+        }
+      });
 
-          if (s.enabled) {
-            file.EmitLine("public const string {0} = \"{0}\";", Path.GetFileNameWithoutExtension(s.path));
-          }
+      file.EmitScope("[System.Obsolete(\"Use BoltMaps instead\")] public enum BoltMapNames", () => {
+        for (int i = 0; i < EditorBuildSettings.scenes.Length; ++i) {
+          var s = EditorBuildSettings.scenes[i];
+          file.EmitLine("{0} = {1},", Path.GetFileNameWithoutExtension(s.path), i);
         }
       });
     }
