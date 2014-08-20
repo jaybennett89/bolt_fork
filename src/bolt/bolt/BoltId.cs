9b407cfcf24f66ddd65f2bb5255905bdc1305dbd
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-08-20 19:37:38 +0200

    stared porting over work for unique ids

---
+++ src/bolt/bolt/BoltId.cs
@@ -0,0 +1,25 @@
+﻿public struct BoltPeerId {
+  public readonly uint id;
+
+  internal BoltPeerId (uint id) {
+    this.id = id;
+  }
+}
+
+public struct BoltObjectId {
+  public readonly uint id;
+
+  internal BoltObjectId (uint id) {
+    this.id = id;
+  }
+}
+
+public struct BoltUniqueId {
+  public readonly BoltPeerId peerId;
+  public readonly BoltObjectId objectId;
+
+  internal BoltUniqueId (BoltPeerId peerId, BoltObjectId objectId) {
+    this.peerId = peerId;
+    this.objectId = objectId;
+  }
+}
