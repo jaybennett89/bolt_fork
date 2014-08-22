1948f2b89ba161be5cd44cf827e9ef9855aa3cad
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-08-22 07:53:16 +0200

    samples assets changes

--- src/bolt.unity/Assets/bolt/samples/vehicles/VehiclesCallbacks.cs
+++ src/bolt.unity/Assets/bolt/samples/vehicles/VehiclesCallbacks.cs
@@ -3,19 +3,19 @@ using System.Collections;
 
 [BoltGlobalBehaviour("Sample_Vehicles")]
 public class VehiclesCallbacks : BoltCallbacks {
-  public override void StartDone () {
-    BoltNetwork.resolveTransform = id => {
-      foreach (var c in FindObjectsOfType<CarControl>()) {
-        if (c.boltState.carId == id) { return c.transform; }
-      }
-
-      return null;
-    };
-
-    BoltNetwork.resolveTransformId = t => {
-      return t.GetComponent<CarControl>().boltState.carId;
-    };
-  }
+  //public override void StartDone () {
+  //  BoltNetwork.resolveTransform = id => {
+  //    foreach (var c in FindObjectsOfType<CarControl>()) {
+  //      if (c.boltState.carId == id) { return c.transform; }
+  //    }
+  //  
+  //    return null;
+  //  };
+  //  
+  //  BoltNetwork.resolveTransformId = t => {
+  //    return t.GetComponent<CarControl>().boltState.carId;
+  //  };
+  //}
 
   public override void Connected (BoltConnection arg) {
     arg.userToken = new CarConnectionToken();
