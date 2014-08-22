8e4abc555c15e5a468c4d38538ddebbf73cf9a3f
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-08-22 08:51:17 +0200

    updated unique id tostring

--- src/bolt/bolt/BoltId.cs
+++ src/bolt/bolt/BoltId.cs
@@ -7,6 +7,10 @@
     this.obj = objectId;
   }
 
+  public override string ToString () {
+    return string.Format("[Id peer={0} object={1}]", peer, obj);
+  }
+
   public static bool operator ==(BoltUniqueId l, BoltUniqueId r) {
     return l.peer == r.peer && l.obj == r.obj;
   }
