386502eedee3e8221d23d0ebf2c9aaacf16bfdfc
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-08-24 09:03:09 +0200

    added correct attribute to TeleportCallbacks

--- src/bolt.unity/Assets/bolt/samples/teleportandelevators/TeleportCallbacks.cs
+++ src/bolt.unity/Assets/bolt/samples/teleportandelevators/TeleportCallbacks.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[BoltGlobalBehaviour("Sample_TeleportAndElevators")]
 public class TeleportCallbacks : BoltCallbacks {
   public override void ControlOfEntityGained (BoltEntity arg) {
     TeleportCamera.instance.target = arg.transform;
