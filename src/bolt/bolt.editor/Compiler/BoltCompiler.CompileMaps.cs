0af44ac6d3c9cf9358866eccc79b45f7c7329eb5
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-09-06 15:32:08 +0200

    map => scene changes

--- src/bolt/bolt.editor/Compiler/BoltCompiler.CompileMaps.cs
+++ src/bolt/bolt.editor/Compiler/BoltCompiler.CompileMaps.cs
@@ -6,14 +6,21 @@ using UnityEngine;
 partial class BoltCompiler {
   public static void CompileMaps (BoltCompilerOperation op) {
     using (BoltSourceFile file = new BoltSourceFile(op.mapsFilePath)) {
-      file.EmitScope("public static class BoltMaps", () => {
+      file.EmitScope("public static class BoltScenes", () => {
         for (int i = 0; i < EditorBuildSettings.scenes.Length; ++i) {
           var s = EditorBuildSettings.scenes[i];
           file.EmitLine("public const string {0} = \"{0}\";", Path.GetFileNameWithoutExtension(s.path));
         }
       });
 
-      file.EmitScope("[System.Obsolete(\"Use BoltMaps instead\")] public enum BoltMapNames", () => {
+      file.EmitScope("[System.Obsolete(\"Use BoltScenes instead\")] public static class BoltMaps", () => {
+        for (int i = 0; i < EditorBuildSettings.scenes.Length; ++i) {
+          var s = EditorBuildSettings.scenes[i];
+          file.EmitLine("public const string {0} = \"{0}\";", Path.GetFileNameWithoutExtension(s.path));
+        }
+      });
+
+      file.EmitScope("[System.Obsolete(\"Use BoltScenes instead\")] public enum BoltMapNames", () => {
         for (int i = 0; i < EditorBuildSettings.scenes.Length; ++i) {
           var s = EditorBuildSettings.scenes[i];
           file.EmitLine("{0} = {1},", Path.GetFileNameWithoutExtension(s.path), i);
