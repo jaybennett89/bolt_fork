6899ce752440fa3a9bdd58b39a80c0a20ab06ac0
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-09-08 10:30:37 +0200

    revamped versioning... again

--- src/bolt/AssemblyInfo.cs
+++ src/bolt/AssemblyInfo.cs
@@ -5,16 +5,13 @@ using System.Runtime.InteropServices;
 #if BOLT_DLL
 [assembly: AssemblyTitle("bolt")]
 [assembly: Guid("e24988b8-bcbc-4e93-a24e-8afc569782fe")]
-[assembly: InternalsVisibleTo("bolt.editor")]
-[assembly: InternalsVisibleTo("bolt.user")]
 #elif BOLT_EDITOR_DLL
 [assembly: AssemblyTitle("bolt.editor")]
 [assembly: Guid("6c922342-f87e-4d21-b4dd-ed7a706e9071")]
 #endif
 
-//uncomment this to access the internals of bolt.dll and bolt.editor.dll in the runtime and editor
-//[assembly: InternalsVisibleTo("Assembly-CSharp")]
-//[assembly: InternalsVisibleTo("Assembly-CSharp-Editor")]
+[assembly: InternalsVisibleTo("bolt.user")]
+[assembly: InternalsVisibleTo("bolt.editor")]
 
 [assembly: AssemblyDescription("Networking middleware for Unity3D")]
 [assembly: AssemblyConfiguration("")]
@@ -24,5 +21,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.3.*")]
-//[assembly: AssemblyFileVersion("0.2.*")]
+[assembly: AssemblyVersion("0.3.1")]
