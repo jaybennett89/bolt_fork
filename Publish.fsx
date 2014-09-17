fc198caf05bbae7eba5b4179fb3072f9d74baaa9
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-09-17 11:01:23 +0200

    tutorial project assets

--- Publish.fsx
+++ Publish.fsx
@@ -21,8 +21,9 @@ let unityPath =
   else 
     @""
     
-let hash = 
-  Fake.Git.Information.getCurrentHash()
+let buildName =
+  let now = System.DateTime.Now
+  sprintf "Bolt_Daily_Y%iM%iD%i" now.Year now.Month now.Day
 
 let packageUnityProject path packageName assetDirs =
 
@@ -46,12 +47,12 @@ Target "CreatePackage" (fun _ ->
     DeleteDir "./upload"
     CreateDir "./upload" 
 
-    packageUnityProject "./src/bolt.unity.tutorial" (sprintf "Bolt_Beta_%s_CompleteWithTutorial" hash) ["bolt"; "bolt_tutorial"; "Gizmos"; "Plugins"]
-    packageUnityProject "./src/bolt.unity.samples" (sprintf "Bolt_Beta_%s_CompleteWithSamples" hash) ["bolt"; "bolt_samples"; "Gizmos"; "Plugins"]
-    packageUnityProject "./src/bolt.unity.samples" (sprintf "Bolt_Beta_%s_InstallerOnly" hash) ["bolt/assemblies/bolt.dll"; "bolt/assemblies/udpkit/udpkit.dll"; "bolt/assemblies/editor/bolt.editor.dll"]
+    packageUnityProject "./src/bolt.unity.tutorial" (sprintf "%s_CompleteWithTutorial" buildName) ["bolt"; "bolt_tutorial"; "Gizmos"; "Plugins"]
+    packageUnityProject "./src/bolt.unity.samples" (sprintf "%s_CompleteWithSamples" buildName) ["bolt"; "bolt_samples"; "Gizmos"; "Plugins"]
+    packageUnityProject "./src/bolt.unity.samples" (sprintf "%s_InstallerOnly" buildName) ["bolt/assemblies/bolt.dll"; "bolt/assemblies/udpkit/udpkit.dll"; "bolt/assemblies/editor/bolt.editor.dll"]
 
     let zipRoot = (directoryInfo "./upload").FullName
-    let zipFileName = (sprintf "./upload/Bolt_Beta_%s.zip" hash)
+    let zipFileName = (sprintf "./upload/%s.zip" buildName)
     let zipFiles = 
       "./upload"
       |> directoryInfo
@@ -65,6 +66,54 @@ Target "UploadPackage" (fun _ ->
   let ascii (s:string) = 
       System.Text.Encoding.ASCII.GetBytes(s)
 
+  let authToken =
+    let auth = (environVar "token") + ":x-oauth-basic"
+    "Basic " + System.Convert.ToBase64String(ascii auth)
+
+
+  let postFile2 (url:string) (data:byte array) =
+    let wc = new System.Net.WebClient();
+    let re = new System.Threading.ManualResetEvent(false)
+
+    wc.Headers.Add("User-Agent",  "BoltEngine/bolt")
+    wc.Headers.Add("Content-Length", data.Length.ToString())
+    wc.Headers.Add("Content-Type", "application/zip")
+    wc.Headers.Add("Authorization", authToken)
+
+    let progress = ref 0L
+
+    wc.UploadDataCompleted.Add (fun args -> re.Set() |> ignore)
+    wc.UploadProgressChanged.Add (fun args -> 
+      System.Console.WriteLine("PROGRESS CHANGED")
+      progress := args.BytesSent
+    )
+
+    wc.UploadDataAsync(new System.Uri(url), data)
+
+    while re.WaitOne(1000) do
+      System.Console.WriteLine(sprintf "Progress %i / %i" !progress data.Length)
+
+  let postFile (url:string) (data:byte array) =
+    System.Console.WriteLine(sprintf "UPLOADING FILE (%i Bytes)" data.Length)
+    System.Console.WriteLine(url)
+
+    let req = System.Net.WebRequest.CreateHttp(url)
+    req.Method <- "POST"
+    req.UserAgent <- "BoltEngine/bolt" 
+    req.ContentLength <- int64 data.Length
+    req.ContentType <- "application/zip"
+    req.Timeout <- 40 * 60 * 1000
+    req.Headers.Add(System.Net.HttpRequestHeader.Authorization, authToken)
+    
+    let stream = req.GetRequestStream()
+
+    stream.Write(data, 0, data.Length)
+    stream.Flush()
+
+    let response = new System.IO.StreamReader(req.GetResponse().GetResponseStream())
+
+    response.ReadToEnd() |> ignore
+
   let createReq (url:string) : System.Net.WebRequest =
     let auth = (environVar "token") + ":x-oauth-basic"
     let authBase64 = System.Convert.ToBase64String(ascii auth)
@@ -74,50 +123,36 @@ Target "UploadPackage" (fun _ ->
     let req = System.Net.WebRequest.CreateHttp(url)
     req.Method <- "POST"
     req.UserAgent<- "BoltEngine/bolt" 
-    req.Headers.Add(System.Net.HttpRequestHeader.Authorization, ("Basic " + authBase64))
+    req.Headers.Add(System.Net.HttpRequestHeader.Authorization, authToken)
     req :> System.Net.WebRequest
-        
+
   let createRelease_URL = "https://api.github.com/repos/BoltEngine/bolt/releases"
   let createRelease_JSON = System.String.Format("""
   {{
-    "tag_name":"build_{0}",
+    "tag_name":"{0}",
     "target_commitish":"master",
-    "name":"build_{0}",
-    "body":"",
+    "name":"{0}",
+    "body":"{0}",
     "draft":false,
     "prerelease":true
   }}
-  """, hash)
+  """, buildName)
   
   let req = createReq createRelease_URL
   let data = ascii createRelease_JSON
   req.GetRequestStream().Write(data, 0, data.Length)
   
-  System.Console.WriteLine(createRelease_URL);
-
   let res = req.GetResponse();
   let resStream = res.GetResponseStream();
   let release = CreateRelease.Load(new System.IO.StreamReader(resStream))
 
-  let uploadAssetURL = sprintf "https://uploads.github.com/repos/BoltEngine/bolt/releases/%A/assets?name=Bolt_Beta_%s.zip" release.Id hash
-  let req = createReq uploadAssetURL
+  let uploadURL = sprintf "https://uploads.github.com/repos/BoltEngine/bolt/releases/%A/assets?name=%s_Binary.zip" release.Id buildName
+  let uploadData = System.IO.File.ReadAllBytes(sprintf "./upload/%s.zip" buildName);
 
-  System.Console.WriteLine(uploadAssetURL);
-
-  // additional headers
-  req.ContentType <- "application/zip"
-  req.Timeout <- 40 * 60 * 1000
-
-  let data = System.IO.File.ReadAllBytes(sprintf "./upload/Bolt_Beta_%s.zip" hash);
-
-  req.GetRequestStream().Write(data, 0, data.Length)
- 
-  let response = new System.IO.StreamReader(req.GetResponse().GetResponseStream());
-
-  response.ReadToEnd() |> ignore
+  postFile uploadURL uploadData
 )
 
-"CreatePackage" 
-  ==> "UploadPackage"
+//"CreatePackage" 
+  //==> "UploadPackage"
 
 Run "UploadPackage"
