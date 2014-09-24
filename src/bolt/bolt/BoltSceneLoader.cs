14f37c0fae20e84271463ec10ce43f67c8e00f8b
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-09-24 14:52:56 +0200

    changes for final 0.3.4 release

--- src/bolt/bolt/BoltSceneLoader.cs
+++ src/bolt/bolt/BoltSceneLoader.cs
@@ -7,22 +7,35 @@ class BoltSceneLoader : MonoBehaviour {
     public AsyncOperation async;
   }
 
+  static int _frameDelay;
   static readonly BoltSingleList<LoadOp> _loadOps = new BoltSingleList<LoadOp>();
-  static internal bool isLoading {
-    get { return _loadOps.count > 0; }
-  }
+  static internal bool isLoading { get { return _loadOps.count > 0; } }
 
-  void Update () {
+  void Update() {
     if (_loadOps.count > 0) {
       if (BoltCore.isUnityPro) {
         LoadAsync();
-      } else {
+      }
+      else {
         Load();
+      }
+    }
+    else {
+      if (_frameDelay > 0) {
+        if (--_frameDelay == 0) {
+          try {
+            BoltCore.LoadMapDoneInternal(_loadOps.first.scene);
+          }
+          finally {
+            _loadOps.RemoveFirst();
+          }
+        }
       }
     }
   }
 
-  void Load () {
+  void Load() {
+
     // notify core of loading
     BoltCore.LoadMapBeginInternal(_loadOps.first.scene);
 
@@ -33,7 +46,7 @@ class BoltSceneLoader : MonoBehaviour {
     Done();
   }
 
-  void LoadAsync () {
+  void LoadAsync() {
     if (_loadOps.first.async == null) {
       // notify core of loading
       BoltCore.LoadMapBeginInternal(_loadOps.first.scene);
@@ -41,26 +54,21 @@ class BoltSceneLoader : MonoBehaviour {
       // begin new async load
       _loadOps.first.async = Application.LoadLevelAsync(_loadOps.first.scene.name);
 
-    } else {
+    }
+    else {
       if (_loadOps.first.async.isDone) {
         Done();
       }
     }
   }
 
-  void Done () {
-    try {
-      // collect all old garbage
-      GC.Collect();
-
-      // invoke to core
-      BoltCore.LoadMapDoneInternal(_loadOps.first.scene);
-    } finally {
-      _loadOps.RemoveFirst();
-    }
+  void Done() {
+    GC.Collect();
+    _frameDelay = 10;
   }
 
-  internal static void Enqueue (Scene scene) {
+  internal static void Enqueue(Scene scene) {
+    _frameDelay = 0;
     _loadOps.AddLast(new LoadOp { scene = scene });
   }
 }
