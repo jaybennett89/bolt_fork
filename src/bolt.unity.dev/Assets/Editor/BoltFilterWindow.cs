2fbe9b8d34c78cc8053e0580ec502e6893a56cec
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-09-25 14:06:59 +0200

    filter

---
+++ src/bolt.unity.dev/Assets/Editor/BoltFilterWindow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using Bolt.Compiler;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+
+public class BoltFilterWindow : BoltWindow {
+  [MenuItem("Window/Bolt Filters")]
+  public static void Open() {
+    BoltFilterWindow w;
+
+    w = EditorWindow.GetWindow<BoltFilterWindow>();
+    w.title = "Bolt Filters";
+    w.name = "Bolt Filters";
+    w.minSize = new Vector2(200, 400);
+    w.Show();
+  }
+
+  void OnGUI() {
+    base.OnGUI();
+
+
+  }
+}
