ccc95ddd0329cfcc33cd9151d574994cf812c1f6
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-10-07 12:43:39 +0200

    merged code from unity dev project

---
+++ src/bolt/bolt.editor/PropertyEditors/PropertyEditorStruct.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Linq;
+using Bolt.Compiler;
+using System;
+
+public class PropertyEditorStruct : PropertyEditor<PropertyTypeStruct> {
+  protected override void Edit(bool array) {
+    BoltEditorGUI.WithLabel("Struct Type", () => {
+      PropertyType.StructGuid = BoltEditorGUI.AssetPopup(BoltWindow.Project.Structs.Cast<AssetDefinition>(), PropertyType.StructGuid, new Guid[] { Asset.Guid });
+    });
+  }
+}
---
+++ src/bolt/bolt.editor/PropertyEditors/PropertyEditorVector.cs
@@ -0,0 +1,11 @@
+﻿using Bolt.Compiler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class PropertyEditorVector : PropertyEditor<PropertyTypeVector> {
+  protected override void Edit(bool array) {
+
+  }
+}
