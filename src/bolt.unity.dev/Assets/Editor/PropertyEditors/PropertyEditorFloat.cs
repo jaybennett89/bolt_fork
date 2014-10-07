a919348d1ed149f9d8e301615f354bb23fa79526
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-10-07 12:43:17 +0200

    unique id stuff

--- src/bolt.unity.dev/Assets/Editor/PropertyEditors/PropertyEditorFloat.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using Bolt.Compiler;
-
-public class PropertyEditorFloat : PropertyEditor<PropertyTypeFloat> {
-  protected override void Edit(bool array) {
-    PropertyType.Compression = BoltEditorGUI.EditFloatCompression(PropertyType.Compression);
-  }
-}
