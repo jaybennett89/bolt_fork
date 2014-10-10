e86dd97cce8ffee20bafcf176714f30b9d679dd5
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-10-10 18:34:56 +0200

    bumped version

--- src/bolt/AssemblyInfo.cs
+++ src/bolt/AssemblyInfo.cs
@@ -25,4 +25,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.4.0.7")]
+[assembly: AssemblyVersion("0.4.0.8")]
