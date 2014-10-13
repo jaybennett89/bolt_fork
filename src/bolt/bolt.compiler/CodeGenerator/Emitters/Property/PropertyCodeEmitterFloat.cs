3bfca29f10857246e7cf9d7b20abef33cf275d99
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-10-13 19:46:53 +0200

    hotfix

--- src/bolt/bolt.compiler/CodeGenerator/Emitters/Property/PropertyCodeEmitterFloat.cs
+++ src/bolt/bolt.compiler/CodeGenerator/Emitters/Property/PropertyCodeEmitterFloat.cs
@@ -21,13 +21,13 @@ namespace Bolt.Compiler {
         propertyData.Add(Decorator.Definition.StateAssetSettings.GetMecanimDataExpression());
       }
 
-      var c = Decorator.PropertyType.Compression;
-      if (c.Enabled && c.BitsRequired != 32) {
-        propertyData.Add(string.Format("new Bolt.FloatCompression {{ Bits = {0}, Shift = {1}f, PackMultiplier = {2}f, ReadMultiplier = {3}f }}", c.BitsRequired, -c.MinValue, 1f / c.Accuracy, c.Accuracy));
-      }
-      else {
-        propertyData.Add(string.Format("new Bolt.FloatCompression {{ Bits = 32 }}"));
-      }
+      //var c = Decorator.PropertyType.Compression;
+      //if (c.Enabled && c.BitsRequired != 32) {
+      //  propertyData.Add(string.Format("new Bolt.FloatCompression {{ Bits = {0}, Shift = {1}f, PackMultiplier = {2}f, ReadMultiplier = {3}f }}", c.BitsRequired, -c.MinValue, 1f / c.Accuracy, c.Accuracy));
+      //}
+      //else {
+      //  propertyData.Add(string.Format("new Bolt.FloatCompression {{ Bits = 32 }}"));
+      //}
 
       return propertyData.ToArray();
     }
