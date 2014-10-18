78389e94526bde1c502d7226fc9fce3c6a678e20
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-10-18 19:29:15 +0200

    fixed http://forum.boltengine.com/viewtopic.php?f=6&t=537

--- src/bolt/bolt/state/State.cs
+++ src/bolt/bolt/state/State.cs
@@ -323,6 +323,9 @@ namespace Bolt {
       // calculate diff mask
       var diff = Diff(Frames.first, DiffFrame);
 
+      // copy data from latest frame to diff buffer
+      Array.Copy(Frames.first.Data, 0, DiffFrame.Data, 0, Frames.first.Data.Length);
+
       // combine with existing masks for proxies
       var it = Entity.Proxies.GetIterator();
 
@@ -337,9 +340,6 @@ namespace Bolt {
           InvokeCallbacksForProperty(MetaData.PropertySerializers[i]);
         }
       }
-
-      // copy data from latest frame to diff buffer
-      Array.Copy(Frames.first.Data, 0, DiffFrame.Data, 0, Frames.first.Data.Length);
     }
 
     void InvokeCallbacksForProperty(PropertySerializer p) {
