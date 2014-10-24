e23cb92c12a4b1461d277a22f10f7e6fc94b6f21
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-10-24 21:19:44 +0200

    started fiddling with integer compression

--- src/bolt/bolt.compiler/CodeGenerator/Emitters/Property/PropertyCodeEmitterInteger.cs
+++ src/bolt/bolt.compiler/CodeGenerator/Emitters/Property/PropertyCodeEmitterInteger.cs
@@ -17,31 +17,12 @@ namespace Bolt.Compiler {
     public override void AddSettingsArgument(List<string> settings) {
       if (Decorator.PropertyType.CompressionEnabled) {
         var pt = Decorator.PropertyType;
-        settings.Add(string.Format("Bolt.PropertyIntCompressionSettings.Create({0}, {1})", BitsRequired(pt.MaxValue - pt.MinValue), -pt.MinValue));
+        settings.Add(string.Format("Bolt.PropertyIntCompressionSettings.Create({0}, {1})", pt.BitsRequired, -pt.MinValue));
       }
       else {
         settings.Add("Bolt.PropertyIntCompressionSettings.Create()");
       }
     }
 
-    static int BitsRequired(int number) {
-      if (number < 0) {
-        return 32;
-      }
-
-      if (number == 0) {
-        return 1;
-      }
-
-      for (int i = 31; i >= 0; --i) {
-        int b = 1 << i;
-
-        if ((number & b) == b) {
-          return i + 1;
-        }
-      }
-
-      throw new Exception();
-    }
   }
 }
--- src/bolt/bolt.compiler/Property/Type/PropertyTypeInteger.cs
+++ src/bolt/bolt.compiler/Property/Type/PropertyTypeInteger.cs
@@ -16,6 +16,36 @@ namespace Bolt.Compiler {
     [ProtoMember(4)]
     public bool CompressionEnabled;
 
+    static int CalculateBits(int number) {
+      if (number < 0) {
+        return 32;
+      }
+
+      if (number == 0) {
+        return 1;
+      }
+
+      for (int i = 31; i >= 0; --i) {
+        int b = 1 << i;
+
+        if ((number & b) == b) {
+          return i + 1;
+        }
+      }
+
+      throw new Exception();
+    }
+
+    public int BitsRequired {
+      get {
+        if (CompressionEnabled) {
+          return CalculateBits(MaxValue - MinValue);
+        }
+
+        return 32;
+      }
+    }
+
     public override bool HasSettings {
       get { return true; }
     }
