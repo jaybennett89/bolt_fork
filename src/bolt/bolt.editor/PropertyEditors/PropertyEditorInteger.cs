8cdbad51f1e658665925af8cd12e3199c33096c2
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-10-25 09:06:42 +0200

    re-enabled int compression

--- src/bolt/bolt.editor/PropertyEditors/PropertyEditorInteger.cs
+++ src/bolt/bolt.editor/PropertyEditors/PropertyEditorInteger.cs
@@ -5,6 +5,22 @@ using UnityEditor;
 
 public class PropertyEditorInteger : PropertyEditor<PropertyTypeInteger> {
   protected override void Edit(bool array) {
+
+    BoltEditorGUI.WithLabel("Compression", () => {
+      PropertyType.CompressionEnabled = BoltEditorGUI.Toggle(PropertyType.CompressionEnabled);
+
+
+      EditorGUI.BeginDisabledGroup(PropertyType.CompressionEnabled == false);
+
+      PropertyType.MinValue = Mathf.Min(BoltEditorGUI.IntFieldOverlay(PropertyType.MinValue, "Min"), PropertyType.MaxValue - 1);
+      PropertyType.MaxValue = Mathf.Max(BoltEditorGUI.IntFieldOverlay(PropertyType.MaxValue, "Max"), PropertyType.MinValue + 1);
+
+      GUILayout.Label("Bits: " + PropertyType.BitsRequired, EditorStyles.miniLabel, GUILayout.ExpandWidth(false));
+
+      EditorGUI.EndDisabledGroup();
+
+    });
+
     //BoltEditorGUI.WithLabel("Min Value", () => { PropertyType.MinValue = EditorGUILayout.IntField(PropertyType.MinValue); });
     //BoltEditorGUI.WithLabel("Max Value", () => { PropertyType.MaxValue = EditorGUILayout.IntField(PropertyType.MaxValue); });
 
