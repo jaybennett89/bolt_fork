ce69a01dfccc442c446255c43ef1d2077c17ad42
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-11-09 08:27:43 +0100

    porting over the back end to use NetworkValue instead

--- src/bolt/bolt/BoltMath.cs
+++ src/bolt/bolt/BoltMath.cs
@@ -209,5 +209,17 @@ namespace Bolt {
 
       throw new Exception();
     }
+
+    public static int PopCount(ulong value) {
+      int count = 0;
+
+      for (int i = 0; i < 32; ++i) {
+        if ((value & (1UL << i)) != 0) {
+          count += 1;
+        }
+      }
+
+      return count;
+    }
   }
 }
