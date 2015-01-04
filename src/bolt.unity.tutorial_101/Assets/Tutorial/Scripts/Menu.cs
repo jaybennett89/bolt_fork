7ad89b8881bf428bca7859d2e145386618eb1d7a
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2015-01-04 12:32:07 +0100

    basic tutorial stuff

--- src/bolt.unity.tutorial_101/Assets/Tutorial/Scripts/Menu.cs
+++ src/bolt.unity.tutorial_101/Assets/Tutorial/Scripts/Menu.cs
@@ -6,13 +6,13 @@ public class Menu : MonoBehaviour {
     GUILayout.BeginArea(new Rect(10, 10, Screen.width - 20, Screen.height - 20));
 
     if (GUILayout.Button("Start Server", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true))) {
-      BoltLauncher.StartServer(UdpKit.UdpEndPoint.Parse("127.0.0.1:27000"));
+      BoltLauncher.StartServer(UdpKit.UdpEndPoint.Parse("192.168.2.173:27000"));
       BoltNetwork.LoadScene("Tutorial1");
     }
 
     if (GUILayout.Button("Start Client", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true))) {
       BoltLauncher.StartClient();
-      BoltNetwork.Connect(UdpKit.UdpEndPoint.Parse("127.0.0.1:27000"));
+      BoltNetwork.Connect(UdpKit.UdpEndPoint.Parse("192.168.2.173:27000"));
     }
 
     GUILayout.EndArea();
