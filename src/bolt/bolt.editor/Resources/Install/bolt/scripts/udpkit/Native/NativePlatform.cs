a6b1403a0334e7672311142ab22339f18339312e
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2015-01-15 22:39:42 +0100

    fixes for android

--- src/bolt/bolt.editor/Resources/Install/bolt/scripts/udpkit/Native/NativePlatform.cs
+++ src/bolt/bolt.editor/Resources/Install/bolt/scripts/udpkit/Native/NativePlatform.cs
@@ -1,126 +1,84 @@
 ﻿﻿#if (UNITY_ANDROID || UNITY_IPHONE) && !UNITY_EDITOR
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using UdpKit;
+using UnityEngine;
+using System.Collections;
 
-public class NativeSocket : UdpKit.UdpPlatformSocket {
-  NativePlatform platform;
+public class NativePlatform : UdpKit.UdpPlatform {
+  static readonly object timeLock = new object();
 
-  IntPtr socket;
-  bool broadcast;
+  public override UdpKit.UdpPlatformSocket CreateSocket() {
+    return new NativeSocket(this);
+  }
 
-  public override bool Broadcast {
-    get {
-      return broadcast;
+  public override UdpKit.UdpIPv4Address GetBroadcastAddress() {
+#if UNITY_IPHONE
+    var addr = NativePInvoke.GetBroadcastAddress();
+    return new UdpKit.UdpIPv4Address((byte) (addr >> 0), (byte) (addr >> 8), (byte) (addr >> 16), (byte) (addr >> 24));
+#elif UNITY_ANDROID
+    return Android.GetBroadcastAddress();
+#else
+    return new UdpKit.UdpIPv4Address(255, 255, 255, 255);
+#endif
+  }
+
+  public override UdpIPv4Address[] ResolveHostAddresses(string host) {
+    throw new System.NotSupportedException("ResolveHostAddress is not supported in iOS and Android");
+  }
+
+  public override System.Collections.Generic.List<UdpKit.UdpPlatformInterface> GetNetworkInterfaces() {
+    throw new System.NotImplementedException();
+  }
+
+  public override uint GetPrecisionTime() {
+    lock (timeLock) {
+      return NativePInvoke.GetPrecisionTime();
     }
-    set {
-      if (value) {
-        NativePInvoke.BroadcastEnable(socket);
+  }
+
+  public override bool SupportsBroadcast {
+    get { return true; }
+  }
+
+  public override bool SupportsMasterServer {
+    get { return true; }
+  }
+
+#if UNITY_ANDROID
+  static class Android {
+    static AndroidJavaObject androidMulticastLock;
+
+    public static UdpIPv4Address GetBroadcastAddress () {
+      AndroidJavaObject activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+      AndroidJavaObject wifi = activity.Call<AndroidJavaObject>("getSystemService", "wifi");
+      AndroidJavaObject dhcp = wifi.Call<AndroidJavaObject>("getDhcpInfo");
+
+      int dhcp_ip = dhcp.Get<int>("ipAddress");
+      int dhcp_mask = dhcp.Get<int>("netmask");
+      int broadcast = (dhcp_ip & dhcp_mask) | ~dhcp_mask;
+      byte[] quads = new byte[4];
+
+      for (int k = 0; k < 4; k++) {
+        quads[k] = (byte) ((broadcast >> k * 8) & 0xFF);
       }
-      else {
-        NativePInvoke.BroadcastDisable(socket);
+
+      return new UdpIPv4Address(quads[0], quads[1], quads[2], quads[3]);
+    }
+
+    public static void AcquireMulticastLock () {
+      AndroidJavaObject activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+      AndroidJavaObject wifi = activity.Call<AndroidJavaObject>("getSystemService", "wifi");
+      androidMulticastLock = wifi.Call<AndroidJavaObject>("createMulticastLock", "udpkit");
+      androidMulticastLock.Call("acquire");
+    }
+
+    public static void ReleaseMulticastLock () {
+      if (androidMulticastLock != null) {
+        androidMulticastLock.Call("release");
+        androidMulticastLock.Dispose();
+        androidMulticastLock = null;
       }
-
-      broadcast = value;
     }
   }
-
-  public override UdpEndPoint EndPoint {
-    get {
-      UdpEndPoint.Native ep = default(UdpEndPoint.Native);
-
-      if (NativePInvoke.GetEndPoint(socket, out ep) == NativePInvoke.UDPKIT_SOCKET_OK) {
-        return ep.AsManaged;
-      }
-
-      return UdpEndPoint.Any;
-    }
-  }
-
-  public override string Error {
-    get { return NativePInvoke.Error_Wrapper(); }
-  }
-
-  public override bool IsBound {
-    get { return EndPoint != UdpEndPoint.Any; }
-  }
-
-  public override UdpKit.UdpPlatform Platform {
-    get { return platform; }
-  }
-
-  public NativeSocket(NativePlatform p) {
-    platform = p;
-    broadcast = false;
-    socket = NativePInvoke.CreateSocket();
-  }
-
-  public override void Bind(UdpKit.UdpEndPoint ep) {
-    CheckResult(NativePInvoke.Bind(socket, ep.AsNative));
-  }
-
-  public override void Close() {
-    CheckResult(NativePInvoke.Close(socket));
-    socket = IntPtr.Zero;
-  }
-
-  public override int RecvFrom(byte[] buffer, int bufferSize, ref UdpKit.UdpEndPoint endpoint) {
-    var sender = default(UdpEndPoint.Native);
-    var bytesReceived = NativePInvoke.RecvFrom(socket, buffer, bufferSize, out sender);
-
-    if (bytesReceived > 0) {
-      endpoint = sender.AsManaged;
-    }
-
-    if (bytesReceived < 0) {
-      UdpLog.Error(Error);
-      bytesReceived = -1;
-    }
-
-    return bytesReceived;
-  }
-
-  public override bool RecvPoll(int timeout) {
-    return CheckResult(NativePInvoke.RecvPoll(socket, timeout));
-  }
-
-  public override bool RecvPoll() {
-    return RecvPoll(0);
-  }
-
-  public override int SendTo(byte[] buffer, int bytesToSend, UdpKit.UdpEndPoint endpoint) {
-    var bytesSent = NativePInvoke.SendTo(socket, buffer, bytesToSend, endpoint.AsNative);
-
-    if (bytesSent >= 0) {
-      return bytesSent;
-    }
-
-    UdpLog.Error(Error);
-    return -1;
-  }
-
-  bool CheckResult(int result) {
-    if (result == NativePInvoke.UDPKIT_SOCKET_OK) {
-      return true;
-    }
-
-    if (result == NativePInvoke.UDPKIT_SOCKET_ERROR) {
-      UdpLog.Error(Error);
-      return false;
-    }
-
-    if (result == NativePInvoke.UDPKIT_SOCKET_NOTVALID) {
-      UdpLog.Error("Invalid socket pointer: {0}", socket);
-      return false;
-    }
-
-    if (result == NativePInvoke.UDPKIT_SOCKET_NODATA) {
-      return false;
-    }
-
-    throw new Exception(string.Format("Unknown return code: {0}", result));
-  }
+#endif
 }
 #endif
