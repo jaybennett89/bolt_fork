6b0e21549d904d71b2ac24b629479ded6c011be0
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2015-01-20 15:34:59 +0100

    fixed version numbers, added version number for F# projects

--- src/bolt/AssemblyInfo.cs
+++ src/bolt/AssemblyInfo.cs
@@ -25,4 +25,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.4.1.8")]
+[assembly: AssemblyVersion("0.4.2.0")]
