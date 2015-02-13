d5f14306069aaa49a7e3796bb23d4804dafa98bd
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2015-02-13 10:34:56 +0100

    files for stresstest and dedicated server sample

---
+++ src/bolt.unity.sample.dedicatedserver/Assets/Sample/ServerCallbacks.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[BoltGlobalBehaviour]
+public class ServerCallbacks : Bolt.GlobalEventListener {
+  public override void Connected(BoltConnection connection) {
+    BoltNetwork.Instantiate(BoltPrefabs.Client, new Vector3(UnityEngine.Random.value * 16f, 0, UnityEngine.Random.value * 16f), Quaternion.identity);
+  }
+
+  public override void BoltStarted() {
+    Console.WriteLine("Server Started On: {0}", BoltNetwork.UdpSocket.LanEndPoint);
+  }
+}
